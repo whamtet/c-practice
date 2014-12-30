a02f3d71962e17cbf7bee0e6a8165c3af830ece2
Matthew Molloy <whamtet@gmail.com>
2014-12-30 09:13:43 +0800

    added ride

--- src/c_practice/core.clj
+++ src/c_practice/core.clj
@@ -3,4 +3,4 @@
 (defn -main [& args]
   (println "copying...")
   (spit "index.html"
-        (format (slurp "index2.html") (slurp "../bushou/data/tuttle.csv"))))
+        (format (slurp "index2.html") (slurp "/Users/matthewmolloy/clojure/bushou/data/tuttle.csv"))))
